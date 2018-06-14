f6da4783baf5e901dec79d51f1efc2b71443c40c
David Gibb <degibb@gmail.com>
2018-06-14 09:41:02 -0700

    renamed resource

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ resource "aws_s3_bucket" "b" {
   bucket_prefix = "sample-bucket-for-pipeline"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "c" {
   bucket_prefix = "another-sample-bucket-for-pipeline"
 }
